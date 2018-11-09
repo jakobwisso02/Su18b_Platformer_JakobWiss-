0d55e741e462723b1c8ab2ef9181783e495f58d3
Jakob Wissö <jakob.wisso@elev.ga.lbs.se>
2018-11-09 15:58:45 +0100

    Lektion 1
    Move left right and jump, ground checker

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 6f;
+    public float jumpSpeed = 12f;
+
+    public GroundChecker groundCheck;
+
+    private Rigidbody2D rbody;
+
+
+    
+    void Start()
+    {
+        rbody = GetComponent<Rigidbody2D>();
+    }
+ 
+    void Update()
+    {
+        rbody.velocity = new Vector2(
+            Input.GetAxisRaw("Horizontal") * moveSpeed, 
+            rbody.velocity.y);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (groundCheck.isGrounded == true)
+            {
+                rbody.velocity = new Vector2(
+                    rbody.velocity.x,
+                    jumpSpeed);
+            }
+        }
+    }
+}
