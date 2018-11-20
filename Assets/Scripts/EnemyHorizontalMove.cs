6dc65c3cb50ac71827b1b267fc5631a288fc932a
Jakob Wissö <jakob.wisso@elev.ga.lbs.se>
2018-11-20 10:51:42 +0100

    Lektion3

---
+++ Assets/Scripts/EnemyHorizontalMove.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHorizontalMove : MonoBehaviour
+{
+    public float speed = 5f;
+    public bool left = true;
+
+    public GroundChecker check;
+
+    private Rigidbody2D rbody;
+
+
+    void Start()
+    {
+        rbody = GetComponent<Rigidbody2D>();
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        left = !left;
+    }
+
+
+    void FixedUpdate()
+    {
+        if (left == true)
+        {       //casting
+            rbody.MovePosition(rbody.position + (-(Vector2)transform.right * Time.deltaTime) * speed);
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        else
+        {
+            rbody.MovePosition(rbody.position + ((Vector2)transform.right * Time.deltaTime) * speed);
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+
+        if (check.isGrounded == false)
+        {
+            left = !left;
+        }
+    }
+}
